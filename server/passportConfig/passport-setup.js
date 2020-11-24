cea646c799683dbd3f0b23d137660fec5ed1d3dc
Matthew Mccarthy <matthewmc96@gmail.com>
2020-11-24 02:58:25 -0600

    getting it

--- server/passportConfig/passport-setup.js
+++ server/passportConfig/passport-setup.js
@@ -3,13 +3,22 @@ const GoogleStrategy = require('passport-google-oauth20')
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user)
+  })
+})
+
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://localhost:3000/auth/google/callback`
-  },
-  function (accessToken, refreshToken, profile, done) {
-    User.findOnCreate({ googleId: profile.id }, function (err, user) {
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOrCreate({ googleId: profile.id }, (err, user) => {
       return done(err, user)
     })
   }
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,42 +1,75 @@
 /** @format */
 require('dotenv').config()
+require('../passportConfig/passport-setup')
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const morgan = require('morgan')
+const protectedRoutes = require('../routes/protected')
+const verifyToken = require('../routes/validate-token')
+const authRoutes = require('../routes/auth')
 
-mongoose.connect(
-// DB connection
- process.env.CONNECTION_STRING,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  () => console.log('🚀 DB Blasting Off')
-)
+const { SERVER_PORT, CONNECTION_STRING, COOKIE_SESSION } = process.env
+const port = SERVER_PORT || 5000
+const uri = CONNECTION_STRING
 
+const app = express()
 
 // Middlewares
-const protectedRoutes = require("../routes/protected")
-const verifyToken = require("../routes/validate-token")
-const authRoutes = require("../routes/auth")
-app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieSession({
+  name: COOKIE_SESSION,
+  keys: ['key1', 'key2']
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-// Route Middlewares
-app.use("/api/user", authRoutes)
-app.use("/api/protected", verifyToken, protectedRoutes)
+// Routes Middlewares
+app.use('/api/user', authRoutes)
+app.use('/api/protected', verifyToken, protectedRoutes)
 
+// Routes
+app.get('/failed', (req, res) => res.send('You failed to log in correctly!'))
+
+app.get('/success',
+  (req, res) => res.send('Toast to success mr. or mrs. ${req.user.email}!'))
 // Google Auth
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }))
+  passport.authenticate('google', { scope: ['profile', 'email'] }))
 
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-
-  res.redirect('/')
+  function (req, res) {
+// If 🚀 Success redirect to specified route
+    res.redirect('/')
   })
 
-// Server Port
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server connected on port: ${ process.env.SERVER_PORT }`)
+// Startup Server and Connect DB
+app.listen(port, (err) => {
+  if (err) console.log('Error in server setup')
+  console.log(`👨 To Infinity & Beyond on Port => ${ port }`)
+
+  try {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: false
+    })
+  } catch (error) {
+    console.error(error)
+  }
+
+  mongoose.connection.on('error', (e) => {
+    console.log('mongo connect error!')
+  })
+  mongoose.connection.on('connected', () => {
+    console.log('🚀 DB Blasting Off')
+  })
 })
+
